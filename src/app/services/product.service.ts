915ff92335a83ff2377145a1e43c7d563b25d175
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2025-03-27 00:06:29 +0100

    enviroment

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,39 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {Category, Product} from '../interfaces/models';
-import {environment} from '../../environments/environment';
+import { Category, Product } from '../interfaces/models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = environment.apiUrl + '/api/products';  // Cambié esto
+  private apiUrl = environment.apiUrl; // Esto se ajusta dependiendo del environment (Producción o Desarrollo)
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
   }
 
   getProductById(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+    return this.http.get<Product>(`${this.apiUrl}/products/${id}`);
   }
 
   addProduct(newProduct: Product): Observable<Product> {
-    return this.http.post<Product>(this.apiUrl, newProduct);
+    return this.http.post<Product>(`${this.apiUrl}/products`, newProduct);
   }
 
   deleteProduct(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/products/${id}`);
   }
+
   updateProduct(id: number, updatedProduct: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.apiUrl}/${id}`, updatedProduct);
+    return this.http.put<Product>(`${this.apiUrl}/products/${id}`, updatedProduct);
   }
+
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
+    return this.http.get<Category[]>(`${this.apiUrl}/products/categories`);
   }
-
-
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://produktmanager-backend.onrender.com'
+  apiUrl: 'https://produktmanager-backend.onrender.com/api'
 };
